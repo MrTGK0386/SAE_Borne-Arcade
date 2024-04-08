2b3da0127811f42adb56a56e2673ae50af76e19c
mrtgk <mrtiti30@gmail.com>
2024-04-08 11:55:24 +0200

    Base server.js + node packages

---
+++ back/server.js
@@ -0,0 +1,38 @@
+const http = require("http");
+const request = require("request");
+const lecteur = require("fs");
+
+var compteur;
+
+var serveur = http.createServer(function (req, res) {
+    // message de retour
+    var pageDemande = req.url;
+    if (pageDemande == '/' || pageDemande == '/index.html') {
+        lecteur.readFile('index.html', 'utf-8', function (err, content) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+            res.end()
+            console.log("Reponse envoyé !");
+        })
+    } else if (pageDemande == '/pouet' || pageDemande == '/funny') {
+        lecteur.readFile('pouet.html', 'utf-8', function (err, content) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(content);
+            res.end();
+            console.log("Reponse envoyé !");
+        })
+    } else if (pageDemande == '/lol') {
+        res.setHeader("Content-Type", "text/html");
+        res.write(`<h1>Hello World! Vous avez vu la page lol ${compteur} fois</h1>`);
+        res.end();
+        console.log("Reponse envoyé !");
+    }
+
+    compteur ++;
+    console.log("Une requete a ete detecte");
+    console.log(req.url);
+}).listen(1234);
+
+//on charge socket.io et on le branche au serveur http en cours
+var sockPrincipale = require("socket.io")(serveur);
+//Quand un client se connecte, on lui crée une socket dédié
