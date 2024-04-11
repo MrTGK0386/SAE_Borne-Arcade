cc3761abafdb91b46baaabe6d14bb46e668d2873
mrtgk <mrtiti30@gmail.com>
2024-04-11 15:34:36 +0200

    Trie des dossier dans les listes et affichage sur la page en question

--- back/server.js
+++ back/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const socket = require("ejs/ejs");
 const app=express();
@@ -10,15 +12,32 @@ app.use(express.urlencoded({extended: true}));
 
 const server = http.createServer(app);
 
+
 app.get("/", (req, res) => {
     borneStatus = "home";
     res.render("index", {borneStatus: borneStatus});
 })
 
 app.post("/list", (req, res) => {
-    borneStatus = "listing";
-    res.render("index", {borneStatus : borneStatus});
-})
+    const folderPath = 'public/games'; // Chemin du dossier avec les jeux
+
+    // Lecture des dossiers dans le dossier principal
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error('Error reading directory:', err);
+            res.status(500).json({ error: 'Internal server error' });
+        } else {
+            // Filtrer les dossiers
+            const folders = files.filter(file => fs.statSync(path.join(folderPath, file)).isDirectory());
+
+            const middleIndex = Math.ceil(folders.length / 2);
+            const firstList = folders.slice(0, middleIndex);
+            const secondList = folders.slice(middleIndex);
+
+            res.render('index', { borneStatus: 'listing', firstList, secondList });
+        }
+    });
+});
 
 
 server.listen(3000, () => {
