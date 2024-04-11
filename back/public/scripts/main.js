c38b8e3342570f1d77ee4ac5d7c8906822775f41
mrtgk <mrtiti30@gmail.com>
2024-04-11 23:22:39 +0200

    Sélection OK Ajout de la fonction play fonctionelle sur les jeux (press enter)

--- back/public/scripts/main.js
+++ back/public/scripts/main.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tiles = document.querySelectorAll('.game-tile');
     let currentTileIndex = -1;
+    const listeHaut = document.querySelectorAll('.li1');
+    const listeBas = document.querySelectorAll('.li2');
+    let currentListe = listeHaut;
+    let lastListe = listeBas;
 
     document.addEventListener('keydown', function (event) {
         if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
@@ -9,16 +12,68 @@ document.addEventListener('DOMContentLoaded', function () {
             if (event.key === 'ArrowLeft') {
                 currentTileIndex = Math.max(currentTileIndex - 1, 0);
             } else if (event.key === 'ArrowRight') {
-                currentTileIndex = Math.min(currentTileIndex + 1, tiles.length - 1);
+                currentTileIndex = Math.min(currentTileIndex + 1, currentListe.length - 1);
             }
 
-            tiles.forEach((tile, index) => {
+            currentListe.forEach((tile, index) => {
                 if (index === currentTileIndex) {
                     tile.classList.add('tiles-active');
                 } else {
                     tile.classList.remove('tiles-active');
                 }
             });
+
+        }
+        if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+            event.preventDefault();
+
+            if (event.key === 'ArrowDown') {
+                currentListe = listeBas;
+                lastListe = listeHaut;
+            } else if (event.key === 'ArrowUp') {
+                currentListe = listeHaut;
+                lastListe = listeBas;
+            }
+
+            currentListe.forEach((tile, index) => {
+                if (index === currentTileIndex) {
+                    tile.classList.add('tiles-active');
+                } else {
+                    tile.classList.remove('tiles-active');
+                }
+            });
+
+            lastListe.forEach((tile, index) => {
+                tile.classList.remove('tiles-active');
+            })
+
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (borneStatus === 'home') {
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Enter') {
+                document.querySelector('#boutonMenu').click();
+            }
+        })
+    }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (borneStatus === 'listing') {
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Enter') {
+               const folderName = document.querySelector('.tiles-active').innerHTML;
+               console.log(folderName);
+               play(folderName);
+            }
+        })
+    }
+})
+
+function play(folder){
+    const gamePath = "/games/"+folder;
+    console.log(gamePath);
+}
